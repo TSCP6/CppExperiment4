43a1c738b4f806067a04dd50de45e20a6bf464ea
TSCP6 <1284127668@qq.com>
2025-11-04 20:45:35 +0800

    use using feature to optimize code

--- src/CmderFactory.hpp
+++ src/CmderFactory.hpp
@@ -7,6 +7,10 @@
 #include "Command.hpp"
 
 namespace adas {
+
+using Cmder = std::function<void(PoseHandler& poseHandler)>;
+using CmderList = std::list<Cmder>;
+
 class CmderFactory final {
   public:
     CmderFactory(void) = default;
@@ -16,12 +20,15 @@ class CmderFactory final {
     CmderFactory &operator=(const CmderFactory) = delete;
 
   public:
-    std::list<std::function<void(PoseHandler &poseHandler)>> GetCmders(const std::string &commands) const noexcept;
+    CmderList GetCmders(const std::string &commands) const noexcept;
 
   private:
-    const std::unordered_map<char, std::function<void(PoseHandler &poseHandler)>> cmderMap{
-        {'M', MoveCommand()}, {'L', TurnLeftCommand()}, {'R', TurnRightCommand()},
-        {'F', FastCommand()}, {'B', ReverseCommand()},
+    const std::unordered_map<char, Cmder> cmderMap{
+        {'M', MoveCommand()}, 
+        {'L', TurnLeftCommand()}, 
+        {'R', TurnRightCommand()},
+        {'F', FastCommand()}, 
+        {'B', ReverseCommand()},
     };
 };
 } // namespace adas
